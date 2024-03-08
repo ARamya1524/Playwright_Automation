61eabd1fa9cd865893041e2bb86db53fd0394297
ARamya1524 <aramya.1524@gmail.con>
2024-03-08 19:23:31 +0530

    Second commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title, @ui', async ({ page }) => {
   await page.goto("https://www.rahulshettyacademy.com/AutomationPractice/");
   await page.getByRole('checkbox').first().click();
 });
